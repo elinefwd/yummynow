59ab0b183bbbff72875e81485a7f6592bfaf8a08
elinefwd <elinefwd@gmail.com>
2023-12-18 22:34:52 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>
+              Yummy now
+          </h1>
+      </div>
+      <div className="card">
+<h1>
+         <button>
+             Login
+         </button>
+            <p></p>
+            <button>
+                Registrate
+            </button>
+</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
